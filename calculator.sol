8aba97a5704f0e147f38eae83d9a235bb1585088
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-11-11 14:18:18 +0530

    Create calculator.sol

---
+++ calculator.sol
@@ -0,0 +1,29 @@
+pragma solidity >=0.4.0 <0.7.0;
+pragma experimental ABIEncoderV2;
+
+contract Calculator {
+
+  uint result= 0;
+
+
+  function getResult() public view returns (uint) {
+    return result;
+  }
+
+  function addToNumber(uint num) public {
+    result += num;
+  }
+
+  function substractNumber(uint num) public {
+    result -= num;
+  }
+
+  function multiplyWithNumber(uint num) public {
+    result *= num;
+  }
+
+  function divideByNumber(uint num) public {
+    result /= num;
+  }
+
+}
